cbecf4daad92edb43f944513ecdd87d4018539b1
APCODERS <apcoders@proway.treina>
2022-05-02 17:04:12 -0300

    mais tratamentos e map

--- lib/model.dart
+++ lib/model.dart
@@ -1,11 +1,36 @@
 enum Sexo { MASCULINO, FEMININO }
 
+Sexo? strToSexo(String? str) {
+  if (str == null) {
+    return null;
+  }
+  switch (str.toUpperCase()) {
+    case 'MASCULINO':
+    case 'M' :
+      return Sexo.MASCULINO;
+    case 'FEMININO':
+    case 'F' :
+      return Sexo.FEMININO;
+  }
+}
+
+
 class Pessoa {
   String? nome;
   String? cpf;
   DateTime? nascimento;
   int? idade;
   Sexo? sexo;
+
+  Pessoa(this.nome, this.cpf, this.nascimento, this.sexo);
+
+  Pessoa.fromMap(Map<String, String> map) {
+    nome = map['nome'];
+    cpf = map['cpf'];
+    nascimento = DateTime.parse(map["nascimento"] ?? "2000-01-01");
+    idade = int.parse(map["idade"] ?? "00");
+    sexo = strToSexo(map['sexo']);
+  }
 
   int? getIdade() {
     if (nascimento == null) {
@@ -21,11 +46,18 @@ class Pessoa {
     return "nome=$nome, cpf=$cpf, nascimento$nascimento, idade=${getIdade()}, "
         "sexo=$sexo";
   }
-
 }
 
 class Programador extends Pessoa {
   double? salario;
+
+  Programador(String? nome, String? cpf, DateTime? nascimento, Sexo? sexo,
+      this.salario)
+      : super(nome, cpf, nascimento, sexo);
+
+  Programador.fromMap(Map<String, String> map) : super.fromMap() {
+    salario = double.tryParse(map[salario] ?? "0.0");
+  }
 
   @override
   String toString() {
@@ -35,12 +67,22 @@ class Programador extends Pessoa {
 }
 
 void mainEntidades() {
-  Pessoa pessoa = Programador();
-  pessoa.nome = "João";
-  pessoa.cpf = "060.275.489-59";
-  pessoa.nascimento = DateTime(2003, 07, 24);
-  pessoa.idade = 18;
-  pessoa.sexo = Sexo.MASCULINO;
+  final map = {
+    'nome': 'Joao',
+    'nascimento': '2003-07-24',
+    'sexo': 'M',
+    'salario': '10000.0'
+  };
+
+  Pessoa pessoa = Programador.fromMap(map);
+
+  // Programador pessoa = Programador();
+  // pessoa.nome = "João";
+  // pessoa.cpf = "060.275.489-59";
+  // pessoa.nascimento = DateTime(2003, 07, 24);
+  // pessoa.idade = 18;
+  // pessoa.sexo = Sexo.MASCULINO;
+  // pessoa.salario = isBoss ? 20000.0 : 10000.0;
 
   print(pessoa.toString());
 }
