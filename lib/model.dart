a55fa3562b5eeb32f8e791e514d11ffc6adf8757
APCODERS <apcoders@proway.treina>
2022-05-02 15:11:05 -0300

    feito metodo: idade de acordo com nasc

--- lib/model.dart
+++ lib/model.dart
@@ -6,26 +6,41 @@ class Pessoa {
   DateTime? nascimento;
   int? idade;
   Sexo? sexo;
-  double? salario;
-  bool? programador;
+
+  int? getIdade() {
+    if (nascimento == null) {
+      return null;
+    }
+    final hoje = DateTime.now();
+    final idade = hoje.difference(nascimento!);
+    return idade.inDays ~/ 365;
+  }
 
   @override
   String toString() {
-    return "nome=$nome, cpf=$cpf, nascimento$nascimento, idade=$idade, "
-        "sexo=$sexo, salario.$salario, programador=$programador";
+    return "nome=$nome, cpf=$cpf, nascimento$nascimento, idade=${getIdade()}, "
+        "sexo=$sexo";
   }
 
 }
 
+class Programador extends Pessoa {
+  double? salario;
+
+  @override
+  String toString() {
+    return "nome=$nome, cpf=$cpf, nascimento$nascimento, idade=$idade, "
+        "sexo=$sexo, salario=$salario";
+  }
+}
+
 void mainEntidades() {
-  Pessoa pessoa = Pessoa();
+  Pessoa pessoa = Programador();
   pessoa.nome = "João";
   pessoa.cpf = "060.275.489-59";
   pessoa.nascimento = DateTime(2003, 07, 24);
   pessoa.idade = 18;
   pessoa.sexo = Sexo.MASCULINO;
-  pessoa.salario = 10000.09;
-  pessoa.programador = true;
 
   print(pessoa.toString());
 }
